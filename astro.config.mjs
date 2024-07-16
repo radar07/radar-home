02a25097543874969fdf36899a0ca828aa70ba19
Pranav RK <pranavrk7@gmail.com>
2024-07-16 19:09:50 +0530

    feat: add vercel config and update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [mdx(), sitemap(), tailwind()]
 })
