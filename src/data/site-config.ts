7244a1271ea838f74027da7155536228edd51113
Pranav RK <pranavrk7@gmail.com>
2024-07-17 07:40:25 +0530

    feat: add socials in the footer

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -6,13 +6,16 @@ export type Link = {
 export type Hero = {
   title: string
   profile: string
+  github: string
+  linkedin: string
+  mail: string
+  x: string
 }
 
 export type SiteConfig = {
   title: string
   description: string
   headerNavLinks: Link[]
-  socialLinks: Link[]
   hero: Hero
 }
 
@@ -33,24 +36,14 @@ const siteConfig: SiteConfig = {
       href: '/projects'
     }
   ],
-  socialLinks: [
-    {
-      text: 'Github',
-      href: 'https://github.com/radar07'
-    },
-    {
-      text: 'LinkedIn',
-      href: 'https://www.linkedin.com/in/pranav-rk-979126173'
-    },
-    {
-      text: 'X',
-      href: 'https://x.com/pranavrk24'
-    }
-  ],
   hero: {
     title: 'Pranav RK',
     profile:
-      '<p>An enthusiastic and experienced software engineer with a deep love for computers, Linux, and open-source software. Proven expertise in designing, developing, and leading software solutions, microservices architecture, and continuous involvement from start to end.</p>'
+      '<p>An enthusiastic and experienced software engineer with a deep love for computers, Linux, and open-source software. Proven expertise in designing, developing, and leading software solutions, microservices architecture, and continuous involvement from start to end.</p>',
+    github: 'https://github.com/radar07',
+    linkedin: 'https://www.linkedin.com/in/pranav-rk-979126173',
+    mail: 'mailto:pranavrk.me@gmail.com',
+    x: 'https://x.com/pranavrk24'
   }
 }
 
